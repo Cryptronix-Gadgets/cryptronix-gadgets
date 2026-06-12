663735d0409a1e76cd2fd594786451fc187dc7dc
Cryptronix-Gadgets <dracogamer303@gmail.com>
2026-06-12 15:41:46 +0600

    Update add_product.php

--- modules/add_product.php
+++ modules/add_product.php
@@ -1,1 +1,24 @@
+<?php
+include('../includes/header.php');
+?>
 
+<h2>Products Management</h2>
+
+<a href="add_product.php">+ Add Product</a>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>ID</th>
+        <th>Product Name</th>
+        <th>SKU</th>
+        <th>Price</th>
+        <th>Stock</th>
+        <th>Action</th>
+    </tr>
+
+    <!-- Products Data Here -->
+</table>
+
+<?php
+include('../includes/footer.php');
+?>
